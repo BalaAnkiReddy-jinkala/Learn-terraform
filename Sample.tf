77ca1d3bd2c0840ce57302e5b1857c6e2509c1b1
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-02-25 19:56:31 +0530

    commit

--- Sample.tf
+++ Sample.tf
@@ -1,6 +1,18 @@
-provider "local" {}
-
-resource "local_file" "foo" {
-  content  = "Hello World"
-  filename = "/tmp/foo.bar"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "Sample" {
+  ami= "ami-05b10e08d247fb927"
+  instance_type = "t2.mirco"
+  tags= {
+    Name= "terraform"
+  }
 }
