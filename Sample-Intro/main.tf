3d5e97a9f75346d1530502689d8b89b5550a1aee
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-03-01 16:17:24 +0530

    commit

--- Sample-Intro/main.tf
+++ Sample-Intro/main.tf
@@ -15,7 +15,22 @@ resource "local_file" "anki" {
 variable "x" {
   default = 10
 }
+variable "y" {
+  default = [1,"anki",4.43,boss]
+}
 
-output "x" {
+variable "z" {
+  default = {
+    name= "happy"
+    age=40
+  }
+}
+output "x1" {
   value = "value of x =${var.x}"
+}
+output "y1" {
+  value = var.y[2]
+}
+output "z" {
+  value = "name=${var.z["name"]},age=${var.z["age"]}"
 }
