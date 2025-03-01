824332eed40ec5ff482447b2385b2616a8bd1bdd
BalaAnkiReddy-Jinkala <jinkalaankireddy27@gmail.com>
2025-03-01 15:56:24 +0530

    commit

---
+++ Sample-Intro/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
+provider "local" {}
+resource "local_file" "anki" {
+  content = "Hello to everyone"
+  filename = "/tmp/bala"
+}
